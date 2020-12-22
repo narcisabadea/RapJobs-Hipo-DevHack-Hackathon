e40697f65458831745de3fe959fbed3d15ac3d42
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2020-12-22 22:25:33 +0200

    NM-55 update

---
+++ src/components/CardResult.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-if="searchType === 'employer'">
+      {{ item }}
+      <span style="opacity: 0.3;color:grey"
+        >- {{ item.Jobs | jobsLength }}
+      </span>
+    </div>
+    <div v-if="searchType === 'jobs'">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardResult",
+  props: {
+    searchType: null,
+    item: null,
+  },
+  data() {
+    return {};
+  },
+  filters: {
+    jobsLength: (value) => {
+      return Object.keys(value).length;
+    },
+  },
+};
+</script>
